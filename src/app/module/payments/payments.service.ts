7c5199da4aad24416941c7c42ff91b51d1ecea00
RuponGomeZ <anthonyrupon0@gmail.com>
2026-09-07 01:45:11 +0600

    create payment session function created

--- src/app/module/payments/payments.service.ts
+++ src/app/module/payments/payments.service.ts
@@ -1,7 +1,66 @@
+import { prisma } from "../../lib/prisma";
+import { stripe } from "../../lib/stripe";
+import { RequestUser } from "../../middleware/checkAuth";
+import { AppError } from "../../utils/AppError";
+import httpStatus from "http-status";
+
 const createPaymentSessionIntoDB = async (
   paymentPayload: any,
-  userId: string,
-) => {};
+  user: RequestUser,
+) => {
+  const { rentalId } = paymentPayload;
+
+  const transaction = await prisma.$transaction(async (tx) => {
+    const findUser = await tx.user.findUnique({
+      where: {
+        id: user.userId,
+      },
+    });
+
+    if (!findUser) {
+      throw new AppError(httpStatus.NOT_FOUND, "User not found");
+    }
+
+    const findRental = await tx.rental.findUnique({
+      where: {
+        id: rentalId,
+      },
+    });
+
+    if (!findRental) {
+      throw new AppError(httpStatus.NOT_FOUND, "Rental not found");
+    }
+
+    if (findRental.id !== user.userId) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        "You are not authorized to make payment for this rental",
+      );
+    }
+
+    if (findRental.rentalStatus !== "APPROVED") {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "Only approved rentals can be paid for",
+      );
+    }
+
+    let stripeCustomerId = findUser.stripeCustomerId;
+
+    if (!stripeCustomerId) {
+      const customer = await stripe.customers.create({
+        email: findUser.email,
+        name: findUser.name,
+        metadata: { userId: findUser.id },
+      });
+      stripeCustomerId = customer.id;
+    }
+
+    // TODO:line 33 assignment 4
+  });
+
+  return transaction;
+};
 
 export const PaymentService = {
   createPaymentSessionIntoDB,
